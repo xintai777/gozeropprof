448ae0fc9695dcec2458993eb8ef37e926bc0745
kongdejun <kongdejun@bz.cn>
2021-08-09 16:00:20 +0800

    modify conifg

--- agent.go
+++ agent.go
@@ -14,14 +14,14 @@ var (
 )
 
 // StartAgent starts a pprof agent.
-func StartAgent(c Config) {
+func StartAgent(host string, port int32) {
 	once.Do(func() {
-		if len(c.Host) == 0 {
+		if len(host) == 0 {
 			return
 		}
 
 		threading.GoSafe(func() {
-			addr := fmt.Sprintf("%s:%d", c.Host, c.Port)
+			addr := fmt.Sprintf("%s:%d", host, port)
 			logx.Infof("Starting pprof agent at %s", addr)
 			if err := http.ListenAndServe(addr, nil); err != nil {
 				logx.Error(err)
--- config.go
+++
@@ -1,7 +0,0 @@
-package go_zero_pprof
-
-// A Config is a pprof config.
-type Config struct {
-	Host string `json:",optional"`
-	Port int    `json:",default=6060"`
-}
